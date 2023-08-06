19bfd8e6b464ada1e842625cbe24d35c760be7f6
Donut05 <87012174+Donut05@users.noreply.github.com>
2023-08-07 01:01:54 +0300

    connector visuals

--- Scripts/interactive/AutoConnector.lua
+++ Scripts/interactive/AutoConnector.lua
@@ -7,70 +7,62 @@ Connector.colorNormal = sm.color.new("f1ca06")
 Connector.colorHighlight = sm.color.new("f6e857")
 
 local defaultDir = sm.vec3.new(0, 0, 1)
-g_prevlength = 0
-
-local function recreateParticles(self)
-    self.effects = {}
-    local loops = (self._end - self.start):length()
-    for i = 1, loops do
-        if i == 1 then
-            self.effects[#self.effects + 1] = sm.effect.createEffect("Connector - Connection_first")
-        elseif i == loops then
-            self.effects[#self.effects + 1] = sm.effect.createEffect("Connector - Connection_last")
-        else
-            self.effects[#self.effects + 1] = sm.effect.createEffect("Connector - Connection")
-        end
-    end
-end
+local segments = 14
 
 function Connector.client_onCreate(self)
     self.pointer = sm.effect.createEffect("Connector - Pointer", self.interactable)
     self.start = sm.vec3.zero()
+    self.effects = {}
+    for i = 1, segments do
+        if i == 1 then
+            self.effects[i] = sm.effect.createEffect("Connector - Connection_first")
+        elseif i == segments then
+            self.effects[i] = sm.effect.createEffect("Connector - Connection_last")
+        else
+            self.effects[i] = sm.effect.createEffect("Connector - Connection")
+        end
+    end
 end
 
 function Connector.client_onUpdate(self, dt)
     local parent = self.interactable:getSingleParent()
     if not parent then
         self.pointer:stop()
+        for j = 1, #self.effects do
+            self.effects[j]:stopImmediate()
+        end
         return
     end
 
-    self._end = self.interactable:getWorldBonePosition("pipe")
-
-    local length = (self._end - self.start):length()
-    if length ~= g_prevlength then
-        print("EEEE")
-        g_prevlength = length
-        recreateParticles(self)
-    end
-
-    local posCache = {} --Generate position cache for rotation
-    local loops = (self._end - self.start):length()
-    for j = 1, loops do
-        posCache[j] = sm.vec3.lerp(self.start, self._end, j / loops)
-    end
-
-    for i = 1, loops do
-        local pos = posCache[i]
-        local effect = self.effects[i]
-        if not effect then return end
-        effect:setPosition(pos)
-
-        local nextPos = posCache[i + 1]
-        if nextPos then
-            effect:setRotation(sm.vec3.getRotation(defaultDir, nextPos - pos))
-        else
-            effect:setRotation(sm.vec3.getRotation(defaultDir, posCache[i - 1] - posCache[i - 2]))
-        end
-        effect:setParameter("Color", self.shape:getColor())
-        if not effect:isPlaying() then effect:start() end
-    end
     if parent:isActive() then
         if not self.pointer:isPlaying() then
             self.pointer:start()
         end
     else
         self.pointer:stop()
+    end
+
+    local pos = self.interactable.shape:transformLocalPoint(sm.vec3.new(0, 0.2, 0.5))
+    --sm.particle.createParticle("construct_welding", pos)
+    local sucess, result = sm.physics.raycast(pos, pos + sm.vec3.new(0, 0, 2.22))
+    if sucess then
+        if result.type == "body" then
+            local trigger = sm.areaTrigger.createBox(sm.vec3.new(0.01, 0.01, 0.01), result.pointWorld)
+            if #trigger:getContents() == 1 then
+                print(trigger:getContents()[1])
+                --print(_G[sm.item.getFeatureData(result:getShapes()[1].interactable.shape.uuid).classname].connectionOutput)
+            end
+        end
+    end
+
+    --if self.interactable.shape.body:isStatic() then return end
+
+    self._end = self.interactable:getWorldBonePosition("pipe")
+    for i = 1, segments do
+        self.effects[i]:setPosition(sm.vec3.lerp(self.start, self._end, i / segments))
+        self.effects[i]:setRotation(sm.vec3.getRotation(defaultDir, self._end - self.start))
+        self.effects[i]:setParameter("Color", self.shape:getColor())
+        if not self.effects[i]:isPlaying() then self.effects[i]:start() end
     end
 end
 
