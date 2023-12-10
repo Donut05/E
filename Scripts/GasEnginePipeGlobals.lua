2d5873a63eabd7e6d4a3b4d2916c998f2c62ecd7
Donut05 <87012174+Donut05@users.noreply.github.com>
2023-12-10 23:21:04 +0300

    Initial commit
    Thanks TRB for pipe system implementation

---
+++ Scripts/GasEnginePipeGlobals.lua
@@ -0,0 +1,10 @@
+G_GAS_ENGINE_PIPES = {
+    sm.uuid.new("07232236-22eb-4912-8774-ab185f368bb9"), --blue_thick_pipe_long
+    sm.uuid.new("0dba257b-b907-4919-baaf-2fefe19f4e24"), --small_pipe_short
+    sm.uuid.new("927d80d8-84ee-44f0-9d8b-cb7a688e0b6c"), --small_pipe_long
+    sm.uuid.new("8024db09-9147-4fe3-a747-00bc30ab724b"), --small_pipe_curve
+    sm.uuid.new(""),
+    sm.uuid.new(""),
+    sm.uuid.new(""),
+    sm.uuid.new("")
+}
