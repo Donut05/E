569d49f3089958716003234e5a3b486ec5fb77d6
Donut05 <87012174+Donut05@users.noreply.github.com>
2024-01-27 03:18:21 +0300

    New autoconnector
    All that remiains is checks to not allow stupid stuff

--- Scripts/interactive/automation/AutoConnector.lua
+++ Scripts/interactive/automation/AutoConnector.lua
@@ -11,14 +11,14 @@ Connector.colorHighlight = sm.color.new("f6e857")
 
 local defaultDir = sm.vec3.new(0, 0, 1)
 local segments = 14
-local reset_data = false
 
 function Connector.client_onCreate(self)
-    --Data
-    self.start, self._end = sm.vec3.zero(), sm.vec3.zero()
+    --Shape data
     self.firstShape, self.secondShape = nil, nil
-    --Flags for stage advancing
-    self.stage, self.resetProgress, self.wasPowered, self.hasAcceptableInteratable, self.isFirstLoopAfterStageSwitch = 0, 0, false, false, true
+    --Flags
+    self.animateBeam, self.firstTime, self.setEndasShape, self.passedStep1 = false, true, false, false
+    --Beam data
+    self.start, self._end = sm.vec3.zero(), sm.vec3.zero()
     --Visual and audio effects
     self.effects = {}
     self.draggingAudioLoop = sm.effect.createEffect("Connector - Connecting_loop")
@@ -38,12 +38,71 @@ function Connector.cl_createBeam(self)
     end
 end
 
+function Connector.cl_stopBeam(self)
+    for j = 1, #self.effects do
+        if not sm.exists(self.effects[j]) then return end
+        self.effects[j]:stopImmediate()
+    end
+end
+
+function Connector.cl_resetData(self)
+    --Shapes
+    self.firstShape, self.secondShape = nil, nil
+    --Beam data
+    self.start, self._end = sm.vec3.zero(), sm.vec3.zero()
+    --Flags
+    self.animateBeam, self.firstTime, self.setEndasShape, self.passedStep1 = false, true, false, false
+end
+
+function Connector.cl_checkForShape(self, isFirst)
+    local pos = self.interactable:getWorldBonePosition("pipe")
+    local pos2 = self.interactable.shape:transformLocalPoint(sm.vec3.new(0, 0.2, 2.72))
+    local sucess, result = sm.physics.raycast(pos, pos2)
+    if sucess then
+        if result.type == "body" then
+            local shape = result:getShape()
+            if sm.exists(shape) and shape.interactable then
+                if isFirst then
+                    print("Selected first shape")
+                    self.firstShape = shape
+                    self.animateBeam = true
+                    self.passedStep1 = true
+                else
+                    print("Selected second shape")
+                    self.secondShape = shape
+                    self.animateBeam = true
+                end
+            else
+                self:cl_resetData()
+            end
+        else
+            self:cl_resetData()
+        end
+    else
+        self:cl_resetData()
+    end
+end
+
+function Connector.cl_visualCheck(self)
+    local pos = self.interactable:getWorldBonePosition("pipe")
+    local pos2 = self.interactable.shape:transformLocalPoint(sm.vec3.new(0, 0.2, 2.72))
+    local sucess, result = sm.physics.raycast(pos, pos2)
+    if sucess then
+        if result.type == "body" then
+            local shape = result:getShape()
+            if sm.exists(shape) and shape.interactable and shape ~= self.firstShape then
+                self.setEndasShape = true
+                self._end = shape.worldPosition
+            end
+        end
+    end
+end
+
 function Connector.client_onFixedUpdate(self, dt)
-    --Reset if we loose the connection
+    --Reset if we loose parent
     local parent = self.interactable:getSingleParent()
     if not parent then
-        sm.gui.displayAlertText("NO PARENT!")
-        self.stage, self.firstShape, self.secondShape, self.start, self._end, self.wasPowered, self.hasAcceptableInteratable, self.isFirstLoopAfterStageSwitch = 0, nil, nil, sm.vec3.zero(), sm.vec3.zero(), false, false, true
+        self.firstShape, self.secondShape, self.animateBeam, self.start, self._end = nil, nil, false, sm.vec3.zero(), sm.vec3.zero()
         self.pointer:stop()
         for j = 1, #self.effects do
             if not sm.exists(self.effects[j]) then return end
@@ -52,172 +111,66 @@ function Connector.client_onFixedUpdate(self, dt)
         return
     end
 
-    sm.gui.displayAlertText("STAGE " .. tostring(self.stage))
+    --Switch pointer on and off
+    if parent:isActive() and not self.pointer:isPlaying() then
+        self.pointer:start()
+    elseif not parent:isActive() and self.pointer:isPlaying() then
+        self.pointer:stop()
+    end
 
-    --Do a raycast all the time that we use a lot later
-    local pos = self.interactable:getWorldBonePosition("pipe")
-    local pos2 = self.interactable.shape:transformLocalPoint(sm.vec3.new(0, 0.2, 2.72))
-    local sucess, result = sm.physics.raycast(pos, pos2)
+    --Selection code
+    --1. Check for the first shape
+    if parent:isActive() and self.firstTime and not self.passedStep1 then
+        self.firstTime = false
+        self:cl_checkForShape(true)
+    end
 
-    if self.stage == 0 then --Stage 0 | Buffer stage to make resetting intuitive
-        if self.resetProgress == 0 then
-            if not parent:isActive() then
-                self.resetProgress = 1
-            end
-        elseif self.resetProgress == 1 then
-            if parent:isActive() then
-                self.stage = 1
-                self.resetProgress = 0
-            end
+    --Reset the variable if fail
+    if self.firstTime and parent:isActive() == false and sm.exists(self.firstShape) == false then
+        self.firstTime = true
+    end
+
+    --2. Check for the second shape, connect if needed and reset data
+    if sm.exists(self.firstShape) and self.passedStep1 and not (parent:isActive()) then
+        self:cl_checkForShape(false)
+        if sm.exists(self.firstShape) and sm.exists(self.secondShape) then
+            local data = {
+                firstShape = self.firstShape,
+                secondShape = self.secondShape
+            }
+            self.network:sendToServer("sv_connect", data)
+            sm.effect.playHostedEffect("Builderguide - Stagecomplete", self.firstShape.interactable)
+            sm.effect.playHostedEffect("Builderguide - Stagecomplete", self.secondShape.interactable)
+            self:cl_resetData()
         end
-    elseif self.stage == 1 then --Stage 1 | Power the part => point at the first shape => unpower the part => we start pulling a conncection
+    end
 
-        --Check parent
-        if parent:isActive() and not self.pointer:isPlaying() then
-            self.pointer:start()
+    --Visualization code
+    if self.animateBeam then
+        --Try to get the data needed to animate
+        if sm.exists(self.firstShape) then
+            self.start = self.firstShape.worldPosition
         end
-        if parent:isActive() then
-            --Remember that we achieved the first half of the rquirements to go to the next step
-            self.wasPowered = true
-        end
-
-        --Use raycast data to find out if we are pointing at an interactible we can connect
-        if sucess then
-            if result.type == "body" then
-                local shape = result:getShape()
-                if sm.exists(shape) and shape.interactable then
-                    --Remember the shape if it passes as acceptable
-                    self.firstShape = shape
-                    self.hasAcceptableInteratable = true
-                    self.start = self.firstShape.worldPosition
-                    if parent:isActive() then
-                        sm.effect.playEffect("Saw - Debris", self.firstShape.worldPosition)
-                    end
-                else
-                    reset_data = true
-                end
-            else
-                reset_data = true
-            end
-        else
-            reset_data = true
-        end
-
-        --Reset data if we move on from an acceptable shape
-        if reset_data then
-            reset_data = false
-            self.firstShape, self.hasAcceptableInteratable = nil, false
-        end
-
-        --Remember that the first loop after a stage switch has passed
-        self.isFirstLoopAfterStageSwitch = false
-
-        --Go to the next stage if we have:
-        --    1. An acceptable shape       2. We WERE powered 3. We are CURRENTLY unpowered
-        if self.hasAcceptableInteratable and self.wasPowered and not parent:isActive() then
-            self.stage = 2
-            self.isFirstLoopAfterStageSwitch = true
-        end
-    elseif self.stage == 2 then --Stage 1 | Start drggaing the connection line => point at the second shape => wait for the part to get powered
-
-        --Reset if any of the vital actors are not present
-        if not sm.exists(self.firstShape) then
-            print("Reset")
-            self.stage, self.firstShape, self.hasAcceptableInteratable, self.isFirstLoopAfterStageSwitch = 0, nil, false, true
-            if self.draggingAudioLoop:isPlaying() then
-                self.draggingAudioLoop:stopImmediate()
-            end
-            return
-        end
-
-        --Start playing audio
-        if not self.draggingAudioLoop:isPlaying() then
-            self.draggingAudioLoop:start()
-        end
-
-        --Set up required data
-        self.start = self.firstShape.worldPosition
-        if self.isFirstLoopAfterStageSwitch then
-            self.isFirstLoopAfterStageSwitch = false
-            self.wasPowered, self.hasAcceptableInteratable = false, false
-        end
-
-       --Use raycast data to find out if we are pointing at an interactible we can connect
-        if sucess then
-            if result.type == "body" then
-                local shape = result:getShape()
-                if sm.exists(shape) and shape.interactable and shape ~= self.firstShape then
-                    --Remember the shape if it passes as acceptable
-                    self.secondShape = shape
-                    self.hasAcceptableInteratable = true
-                    self._end = self.secondShape.worldPosition
-                    sm.effect.playEffect("Saw - Debris", self.secondShape.worldPosition)
-                else
-                    reset_data = true
-                end
-            else
-                reset_data = true
-            end
-        else
-            reset_data = true
-        end
-
-        --Reset data if we move on from an acceptable shape
-        if reset_data then
-            reset_data = false
+        self:cl_visualCheck()
+        if not self.setEndasShape then
             self._end = self.interactable:getWorldBonePosition("pipe")
-            self.secondShape, self.hasAcceptableInteratable = nil, false
+        else
+            self.setEndasShape = false
         end
 
-        --Create and update the connection beam
-        if self._end ~= sm.vec3.zero() and self.start ~= sm.vec3.zero() then
-            for i = 1, segments do
-                if not sm.exists(self.effects[i]) then self:cl_createBeam() return end
-                self.effects[i]:setPosition(sm.vec3.lerp(self.start, self._end, i / segments))
-                self.effects[i]:setRotation(sm.vec3.getRotation(defaultDir, self._end - self.start))
-                self.effects[i]:setParameter("Color", self.shape:getColor())
-                if not self.effects[i]:isPlaying() then self.effects[i]:start() end
-            end
+        --Don't do anything if we don't have all of the nececary data or data will make the algorithm shit itself
+        if not (self.start) or not (self._end) or self._end - self.start == sm.vec3.zero() then return end
+
+        --Set and update the connection beam
+        for i = 1, segments do
+            if not sm.exists(self.effects[i]) then self:cl_createBeam() return end
+            self.effects[i]:setPosition(sm.vec3.lerp(self.start, self._end, i / segments))
+            self.effects[i]:setRotation(sm.vec3.getRotation(defaultDir, self._end - self.start))
+            self.effects[i]:setParameter("Color", self.shape:getColor())
+            if not self.effects[i]:isPlaying() then self.effects[i]:start() end
         end
-
-        --Go to the next stage if we have:
-        --    1. An acceptable shape        2. Parent is active
-        if self.hasAcceptableInteratable and parent:isActive() then
-            self.stage = 3
-            self.isFirstLoopAfterStageSwitch = true
-        end
-    elseif self.stage == 3 then --Stage 3 | Connect the two interactibles
-
-        --Stop audio
-        if self.draggingAudioLoop:isPlaying() then
-            self.draggingAudioLoop:stopImmediate()
-        end
-
-        --Stop the pointer
-        if self.pointer:isPlaying() then
-            self.pointer:stop()
-        end
-
-        --Stop the beam
-        for _, effect in pairs(self.effects) do
-            effect:stopImmediate()
-            effect:destroy()
-        end
-
-        --Connect the two interactables
-        local data = {
-            firstShape = self.firstShape,
-            secondShape = self.secondShape
-        }
-        self.network:sendToServer("sv_connect", data)
-
-        --Play confimation effects on both
-        sm.effect.playHostedEffect("Builderguide - Stagecomplete", self.firstShape.interactable)
-        sm.effect.playHostedEffect("Builderguide - Stagecomplete", self.secondShape.interactable)
-
-        --Reset
-        self.stage, self.firstShape, self.secondShape, self.start, self._end, self.wasPowered, self.hasAcceptableInteratable = 0, nil, nil, sm.vec3.zero(), sm.vec3.zero(), false, false
-        return
+    else
+        self:cl_stopBeam()
     end
 end
 
@@ -226,12 +179,12 @@ function Connector.sv_connect(self, data)
 end
 
 function Connector.client_onRefresh(self)
-    print("REFRESHED")
-    for _, effect in pairs(self.effects) do
-        if sm.exists(effect) then
-            effect:stopImmediate()
-            effect:destroy()
-        end
-    end
+    print("[E mod] (Autoconnnector) Refreshed")
+    self:cl_stopBeam()
+    self:cl_resetData()
     Connector.client_onCreate(self)
+end
+
+function Connector.client_onDestroy(self)
+    self:cl_stopBeam()
 end
